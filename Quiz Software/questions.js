70e5b339888ddf7ca6dd6b46235fd18f3f70816a
Abhiranda <113504478+Abhiranda@users.noreply.github.com>
2024-02-10 17:53:45 +0530

    Add files via upload

---
+++ Quiz Software/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "which of the following attributes is used to add link to any element?",
+        answer: "href",
+        options: [
+            "link",
+            "href",
+            "ref",
+            "newref"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does html is stand for?",
+        answer: "hyper text markup language",
+        options : [
+            "hyper text markup language",
+            "hyper text pre processor",
+            "hyper text multiple language",
+            "hyper tool multi programing"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does html is stand for?",
+        answer: "hyper text markup language",
+        options : [
+            "hyper text markup language",
+            "hyper text pre processor",
+            "hyper text multiple language",
+            "hyper tool multi programing"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does html is stand for?",
+        answer: "hyper text markup language",
+        options : [
+            "hyper text markup language",
+            "hyper text pre processor",
+            "hyper text multiple language",
+            "hyper tool multi programing"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does html is stand for?",
+        answer: "hyper text markup language",
+        options : [
+            "hyper text markup language",
+            "hyper text pre processor",
+            "hyper text multiple language",
+            "hyper tool multi programing"
+        ]
+    }
+] 
